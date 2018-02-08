a61fd4e669b60fa0dc68a945b50b08f654dec40c
alex_trask <Sarlack_Work@mail.ru>
2018-02-08 20:46:03 +0300

    Готова форма и логика авторизации

---
+++ includes/db.php
@@ -0,0 +1,6 @@
+<?php
+    require 'libphp/rb.php';
+    R::setup( 'mysql:host=localhost;dbname=users', 'root', '' );
+    
+        session_start();
+
--- index.php
+++ index.php
@@ -5,6 +5,9 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+    <?php
+    require 'includes/db.php';
+    ?>
     <head>
         <meta charset="UTF-8">
         <title>Студи авторской посуды "Добрый кот"</title>
@@ -31,39 +34,37 @@ and open the template in the editor.
                             <div id="signup">   
                                 <h1>Авторизация</h1>
 
-                                <form action="/" method="post">
-
-                                    <div class="top-row">
-                                        <div class="field-wrap">
-                                            <label>
-                                                Имя<span class="req">*</span>
-                                            </label>
-                                            <input type="text" required autocomplete="off" />
-                                        </div>
-
-                                        <div class="field-wrap">
-                                            <label>
-                                                Фамилия<span class="req">*</span>
-                                            </label>
-                                            <input type="text"required autocomplete="off"/>
-                                        </div>
-                                    </div>
+                                <form action="index.php" method="post">
 
                                     <div class="field-wrap">
                                         <label>
-                                            Электронная почта (Email)<span class="req">*</span>
+                                            Логин<span class="req">*</span>
                                         </label>
-                                        <input type="email"required autocomplete="off"/>
+                                        <input type="text" name="login" required autocomplete="off" />
                                     </div>
 
                                     <div class="field-wrap">
                                         <label>
                                             Пароль<span class="req">*</span>
                                         </label>
-                                        <input type="password"required autocomplete="off"/>
+                                        <input type="password" name="password" required autocomplete="off"/>
                                     </div>
 
-                                    <button type="submit" class="button button-block"/>Вперед!</button>
+                                    <div class="field-wrap">
+                                        <label>
+                                            Подтверждние пароля<span class="req">*</span>
+                                        </label>
+                                        <input type="password" name="confirm_password" required autocomplete="off"/>
+                                    </div>
+
+                                    <div class="field-wrap">
+                                        <label>
+                                            Электронная почта (Email)<span class="req">*</span>
+                                        </label>
+                                        <input type="email" name="email" required autocomplete="off"/>
+                                    </div>
+
+                                    <button type="submit" name="do_singup" class="button button-block"/>Вперед!</button>
 
                                 </form>
 
@@ -72,25 +73,25 @@ and open the template in the editor.
                             <div id="login">   
                                 <h1>С возвращением!</h1>
 
-                                <form action="/" method="post">
+                                <form action="index.php" method="post">
 
                                     <div class="field-wrap">
                                         <label>
-                                            Электронная почта (Email)<span class="req">*</span>
+                                            Логин<span class="req">*</span>
                                         </label>
-                                        <input type="email"required autocomplete="off"/>
+                                        <input type="text" name="login" required autocomplete="off"/>
                                     </div>
 
                                     <div class="field-wrap">
                                         <label>
                                             Пароль<span class="req">*</span>
                                         </label>
-                                        <input type="password"required autocomplete="off"/>
+                                        <input type="password" name="password" required autocomplete="off"/>
                                     </div>
 
                                     <!--<p class="forgot"><a href="#">Забыли пароль?</a></p>-->
 
-                                    <button class="button button-block"/>Войти</button>
+                                    <button type="submit" name="do_login" class="button button-block"/>Войти</button>
 
                                 </form>
 
@@ -106,7 +107,65 @@ and open the template in the editor.
 
 
         <?php
-        print_r('Привет из Net Beance');
+        
+        $data = $_POST;
+        if (isset($data['do_singup'])) {
+
+            $errors = array();
+            
+            if (R::count('usertable', 'login = ?', array($data['login'])) > 0) {
+                $errors[] = 'Пользователь с таким логином уже существует!';
+            }
+            
+            if (R::count('usertable', 'email = ?', array($data['email'])) > 0) {
+                $errors[] = 'Пользователь с таким email уже существует!';
+            }
+
+            if (empty($errors)) {
+                $user = R::dispense('usertable');
+                $user->login = $data['login'];
+                $user->password = password_hash($data['password'], PASSWORD_DEFAULT);
+                $user->email = $data['email'];
+                R::store($user);
+                echo '<div style="color: green;">Вы успешно зарегистрированы!</div><hr>';
+            }
+            else {
+                echo '<div style="color: red;">'. array_shift($errors).'</div><hr>';
+            }
+        }
+        
+        if (isset($data['do_login'])) {
+            
+            $errors = array();
+            
+            $user = R::findOne('usertable', 'login = ?', array($data['login']));  
+            if( $user ){
+                if (password_verify($data['password'], $user->password)){
+                    $_SESSION['logged_user'] = $user;
+                    // 'Все хорошо!';
+                    echo '<div style="color: green;">Вы авторизованы!</div><hr>';
+                } else {
+                    $errors[] = 'Пароль введен неверно!';
+                }
+            } else {
+                $errors[] = 'Пользователь с таким логином не найден!';
+            }
+            if ( !empty($errors) ) {
+                echo '<div style="color: red;">'. array_shift($errors).'</div><hr>';
+            }
+        }
+        
+        if(isset($_SESSION['logged_user'])){
+            echo '<h2 style="color: green;">Пользователь авторизован</h2>';
+        } else {
+            echo '<h2 style="color: red;">Пользователь не авторизован</h2>';
+        }
+        
+        //print_r('Привет из Net Beance');
+        
         ?>
+        
+        
+        
     </body>
 </html>
