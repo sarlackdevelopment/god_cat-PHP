4c05b756181f9d5ed692916304b5ab3347d5735e
alex_trask <Sarlack_Work@mail.ru>
2018-02-27 18:39:23 +0300

    Усовершенствовал админскую панель. На полпути к интерактивному добавлению картинок.

--- AdminPanel.php
+++ AdminPanel.php
@@ -26,74 +26,77 @@
             .tftable tr:hover {background-color:#ffffff;}
         </style>
 
-<!--        <table class="tftable" border="1">
-            <tr><th>Header 1</th><th>Header 2</th><th>Header 3</th><th>Header 4</th><th>Header 5</th></tr>
-            <tr><td>Строка:1 Столбец:1</td><td>Строка:1 Столбец:2</td><td>Строка:1 Столбец:3</td><td>Строка:1 Столбец:4</td><td>Строка:1 Столбец:5</td></tr>
-            <tr><td>Строка:2 Столбец:1</td><td>Строка:2 Столбец:2</td><td>Строка:2 Столбец:3</td><td>Строка:2 Столбец:4</td><td>Строка:2 Столбец:5</td></tr>
-            <tr><td>Строка:3 Столбец:1</td><td>Строка:3 Столбец:2</td><td>Строка:3 Столбец:3</td><td>Строка:3 Столбец:4</td><td>Строка:3 Столбец:5</td></tr>
-            <tr><td>Строка:4 Столбец:1</td><td>Строка:4 Столбец:2</td><td>Строка:4 Столбец:3</td><td>Строка:4 Столбец:4</td><td>Строка:4 Столбец:5</td></tr>
-            <tr><td>Строка:5 Столбец:1</td><td>Строка:5 Столбец:2</td><td>Строка:5 Столбец:3</td><td>Строка:5 Столбец:4</td><td>Строка:5 Столбец:5</td></tr>
-            <tr><td>Строка:6 Столбец:1</td><td>Строка:6 Столбец:2</td><td>Строка:6 Столбец:3</td><td>Строка:6 Столбец:4</td><td>Строка:6 Столбец:5</td></tr>
-        </table>-->
-
         <!--
         1. Разложить каталоги по строкам.
         2. Разложить содержимое каталогов по столбцам в виде ПолныйПутьКФайлу.
-        3. Завести отдельный css для админской панели.
+        3. У каждого заголовка столбца вписать форму добавления.
+        4. Завести отдельный css для админской панели.
         -->
+        <div >
+            <form action="gallery.php" method="post" enctype="multipart/form-data">                      
+                <input type="file" multiple name="image" class="load-file-buttons">
+                <input type="submit" value="Добавить выбранное в:" class="load-file-buttons"> 
+                <select class="load-file-buttons">
+                    <?php
+                    $allCategory = getAllCategory();
+                    foreach ($allCategory as $currentCategory) {
+
+                        $nameCategory = $currentCategory->name;
+                        echo '<option>' . $nameCategory . '</option>';
+                    }
+                    ?> 
+                </select>
+            </form>           
+        </div>
         
-        <table class="tftable" border="1">
-            
-            <?php 
-            
-            echo '<tr>';
-            
-            $allCategory = getAllCategory();
-            foreach ($allCategory as $currentCategory) {
-       
-                $nameCategory = $currentCategory->name;
-                echo '<th>' . $nameCategory . '</th>';
-                
-            }
-            
-            echo '</tr>';
-            
-            foreach ($allCategory as $currentCategory) {
-                
+        <div>
+
+            <table class="tftable" border="3">
+
+                <?php
                 echo '<tr>';
-                
-                $nameCategory = $currentCategory->name;
-                $allImageByCategory = getAllImageByCategory($nameCategory);
-                
-                foreach ($allImageByCategory as $currentImage) {
-                    echo '<td>' . $currentImage->path . '</td>';
+
+                $allCategory = getAllCategory();
+                foreach ($allCategory as $currentCategory) {
+
+                    $nameCategory = $currentCategory->name;
+                    echo '<th>' . $nameCategory . '</th>';
                 }
-                
+
                 echo '</tr>';
-                
-            }
+
+                foreach ($allCategory as $currentCategory) {
+
+                    echo '<tr>';
+
+                    $nameCategory = $currentCategory->name;
+                    $allImageByCategory = getAllImageByCategory($nameCategory);
+
+                    foreach ($allImageByCategory as $currentImage) {
+                        
+                        $path = $currentImage->path; 
+                        $alt = "Изображение отсутствует"; 
+                        
+                        echo "
+                        <td>
+                            <div style='display: flex; justify-content: center; flex-wrap: wrap;'>
+                                <div style='margin: 3px'>
+                                    <img src=$path width=150px height=100px alt=$alt>
+                                </div>
+                                <div style='margin: 3px'>
+                                    <img src=$path width=150px height=100px alt=$alt>
+                                </div>
+                            </div>
+                        </td>";
+                    }
+
+                    echo '</tr>';
+                }
+                ?>
+
+            </table>
             
-            ?>
-            
-            <!--<tr><th>Header 1</th><th>Header 2</th></tr>
-            <tr>
-                <td>
-                    <form action="gallery.php" method="post" enctype="multipart/form-data">                      
-                        <input type="file" name="image" class="load-file-buttons">
-                        <input type="submit" class="load-file-buttons">                                               
-                    </form>
-                </td>
-                <td>Строка:1 Столбец:2</td>
-            </tr>
-            <tr>
-                <td>Строка:2 Столбец:1</td>
-                <td>Строка:2 Столбец:2</td>
-            </tr>
-            <tr>
-                <td>Строка:3 Столбец:1</td>
-                <td>Строка:3 Столбец:2</td>
-            </tr>-->
-        </table>
+        </div>
         
     </body>
 </html>
